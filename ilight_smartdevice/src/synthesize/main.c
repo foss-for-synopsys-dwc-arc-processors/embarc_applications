59622b43f11f08116a85a05fa40a132b0b150ac2
Mr.WangS <13659829769@163.com>
2017-09-05 15:34:25 +0800

    application:modify coding style in main,makefile,task.c and so on

--- ilight_smartdevice/src/synthesize/main.c
+++ ilight_smartdevice/src/synthesize/main.c
@@ -1,3 +1,80 @@
+/* Copyright (c) 2017, Synopsys, Inc. All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+
+ * 1) Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+
+ * 3) Neither the name of the Synopsys, Inc., nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \version 2017.09
+ * \date 2017-09-1
+ * \author Mr.WangS(mrwangshuai@hust.edu.cn)
+--------------------------------------------- */
+
+/**
+ * \defgroup	EMBARC_APP_FREERTOS_IOT_ILIGHT_SMARTDEVICE	embARC IOT iLight synthesize
+ * \ingroup	EMBARC_APPS_TOTAL
+ * \ingroup	EMBARC_APPS_OS_FREERTOS
+ * \ingroup	EMBARC_APPS_MID_LWIP
+ * \ingroup	EMBARC_APPS_MID_WAKAAMA
+ * \ingroup	EMBARC_APPS_MID_FATFS
+ * \ingroup	EMBARC_APPS_MID_AWS
+ * \brief	embARC IOT iLight smartdevice
+ *
+ * \details
+ * ### Extra Required Tools
+ *
+ * ### Extra Required Peripherals
+ *     [PCF8563](http://www.nxp.com/products/interfaces/ic-bus/ic-real-time-clocks-rtc/real-time-clock-calendar:PCF8563?fsrch=1&sr=1&pageNum=1)
+ *     [MPU6050](https://www.invensense.com/products/motion-tracking/6-axis/mpu-6050/)
+ *     [HM-10](http://www.huamaosoft.cn/bluetooth.asp?id=0)
+ *     [MLX90614](http://www.nxp.com/products/interfaces/ic-bus/ic-dacs-and-adcs/8-bit-a-d-and-d-a-converter:PCF8591?lang_cd=zh-Hans)
+ *
+ * ### Design Concept
+ *     All sensor modules should be connected to \ref EMBARC_BOARD_CONNECTION "EMSK".
+ *     This application is designed to make a smart device to facilitate the daily life.
+ *
+ * ### Usage Manual
+ *
+ *     It can report the following information to the iLight.
+ *     - the Motion Intensity (uint16_t) - Connected to J3 (Based on Acceleration sensor)
+ *     - the MIC (uint8_t) - Connected to J3 (Based on AD conversion)
+ *     - the Middle lights(uint16_t) - Connected to J1 
+ *     - the Side lights (uint64_t) - Connected to J4 (Based on Acceleration sensor)
+ *
+ * ### Extra Comments
+ *
+ */
+
+/**
+ * \file
+ * \ingroup	EMBARC_APP_FREERTOS_IOT_IBABY_SMARTHOME_MULTINODE_WEARABLE_NODE
+ * \brief	main source of iLight Smartdevice 
+ */
+
+/**
+ * \addtogroup	EMBARC_APP_FREERTOS_IOT_ILIGHT_SMARTDEVICE
+ * @{
+ */
 /* embARC HAL */
 #include "embARC.h"
 #include "embARC_debug.h"
@@ -6,20 +83,20 @@
 #include "mpu6050.h"
 #include "imu.h"
 #include "scope.h"
-#include "blu_int.h"
+#include "ble_int.h"
 
 
-
+// #define SCOPE_EN	(1) 
 void main()
 {
 	#ifdef SCOPE_EN
 		scope_init(UART_BAUDRATE_115200);
 	#else
-		blu_uart_init(UART_BAUDRATE_9600);
+		ble_uart_init(UART_BAUDRATE_9600);
 	#endif
-	led_ctr_gpio_init();
+	light_ctr_init();
 	mic_init();
-    mpu6050_init(2,3);
+	mpu6050_init(2,3);
 	task_init();
 	while(1) 
 	{
--- ilight_smartdevice/src/synthesize/task.c
+++ ilight_smartdevice/src/synthesize/task.c
@@ -1,10 +1,48 @@
+/* ------------------------------------------
+ * Copyright (c) 2017, Synopsys, Inc. All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+
+ * 1) Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+
+ * 3) Neither the name of the Synopsys, Inc., nor the names of its contributors may
+ * be used
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \version 2017.07
+ * \date 2017-07-26
+ * \author Jingru Wang(nuannuan12016@outlook.com)
+--------------------------------------------- */
+
+/**
+ * \file
+ * \ingroup	EMBARC_APP_FREERTOS_IOT_ILIGHT_SMARTDEVICE
+ * \brief	task in FreeRTOS
+ */
+/* custom HAL */
 #include "task.h"
 
 #define led_display_task_prioroties 		( (configMAX_PRIORITIES - 1) | portPRIVILEGE_BIT )
 #define pattern_change_task_prioroties 		( (configMAX_PRIORITIES - 2) | portPRIVILEGE_BIT )
-#define light_mode_task_prioroties 			( (configMAX_PRIORITIES - 3) | portPRIVILEGE_BIT )
+#define light_mode_task_prioroties 		( (configMAX_PRIORITIES - 3) | portPRIVILEGE_BIT )
 #define light_mode_music_task_prioroties 	( (configMAX_PRIORITIES - 3) | portPRIVILEGE_BIT )
-#define mpu_task_prioroties					( (configMAX_PRIORITIES - 4) | portPRIVILEGE_BIT )
+#define mpu_task_prioroties			( (configMAX_PRIORITIES - 4) | portPRIVILEGE_BIT )
 #define light_mode_alarm_task_prioroties	( (configMAX_PRIORITIES - 3) | portPRIVILEGE_BIT )
 
 static void led_display_task(void * par);
@@ -35,10 +73,8 @@ static uint32_t time_clock;
 static volatile uint32_t start = 0;
 static uint32_t perf_id = 0xFF;
 
-/**
- * \brief  led display task in FreeRTOS
- * \details led display.
- * \param[in] *par
+/** 
+ * \brief  	task for led displaying.
  */
 static void led_display_task(void * par)
 {
@@ -46,14 +82,14 @@ static void led_display_task(void * par)
 	int led_num;
 
 	led_queue = xQueueCreate( 1, sizeof(RGB_T *) );
-	if( led_queue == NULL ){
+	if (led_queue == NULL) {
 		EMBARC_PRINTF("Failed to create the led queue.\r\n");
 		vTaskSuspend(NULL); 
 	}
 
 	EMBARC_PRINTF("Light ready\r\n");
 	while(1) {
-		xQueueReceive( led_queue, &pled_rgb, (TickType_t) LED_WAIT_TIME_MAX );
+		xQueueReceive(led_queue,&pled_rgb,(TickType_t)LED_WAIT_TIME_MAX);
 		taskENTER_CRITICAL();
 		delay_10us(5);
 		for (led_num = 0; led_num < light_num; led_num++ ) {
@@ -62,10 +98,9 @@ static void led_display_task(void * par)
 		taskEXIT_CRITICAL();
 	}
 }
-/**
- * \brief  pattern change task in FreeRTOS
- * \details Pattern change.
- * \param[in] *par 
+
+/**  
+ *\brief 	pattern change task in FreeRTOS.
  */
 static void pattern_change_task(void * par)
 {
@@ -75,41 +110,38 @@ static void pattern_change_task(void * par)
 
 	recognition_semaphore = xSemaphoreCreateBinary();
 	if (recognition_semaphore == NULL) {
-        EMBARC_PRINTF("Failed to create the recognition semaphore.\r\n");
+		EMBARC_PRINTF("Failed to create the recognition semaphore.\r\n");
 		vTaskSuspend(NULL); 
-    }
-
-	mode_info_queue = xQueueCreate( 1, sizeof( mode_info ) );
+	}
+	mode_info_queue = xQueueCreate (1, sizeof(mode_info));
 	if (mode_info_queue == NULL) {
 		EMBARC_PRINTF("Failed to create the mode information queue.\r\n");
 		vTaskSuspend(NULL); 
 	}
-
 	xQueueOverwrite(mode_info_queue, &mode_info_now);
-
-	EMBARC_PRINTF("pattern change ready\r\n");
+	EMBARC_PRINTF("Pattern change ready!\r\n");
 	vTaskSuspend(NULL);
 	while(1) {
 
-		if (mode_info_old.mode == LIGHT_MODE_MUSIC)
+		if (mode_info_old.mode == LIGHT_MODE_MUSIC) {
 			vTaskSuspend(light_mode_music_task_handle);
-		else if (mode_info_old.mode >= MIN_MODE && mode_info_old.mode <= MAX_MODE)
+		} else if (mode_info_old.mode >= MIN_MODE && mode_info_old.mode <= MAX_MODE) {
 			vTaskSuspend(light_mode_task_handle);
-		else
-			EMBARC_PRINTF("mode error.\r\n");
-
-		if (xSemaphoreTake( recognition_semaphore, (TickType_t) 0 ) == pdTRUE) {
-			EMBARC_PRINTF("recognitioning\r\n");
+		} else {
+			EMBARC_PRINTF("Mode error.\r\n");
+		}
+		if (xSemaphoreTake(recognition_semaphore, (TickType_t)0) == pdTRUE) {
+			EMBARC_PRINTF("Recognitioning\r\n");
 			vTaskSuspend(NULL);
 		}
 		light_ctr_reset();
-		xQueuePeek(mode_info_queue, &mode_info_now , (TickType_t) 0 );
+		xQueuePeek(mode_info_queue, &mode_info_now , (TickType_t)0);
 		/* restart light mode task */
 		if (mode_info_now.mode == LIGHT_MODE_TIMING_FIT) {
 			time_clock = mode_info_now.time;
 			mode_info_now.mode = mode_info_old.mode;
 		}
-		else if ( mode_info_now.mode != mode_info_old.mode 
+		else if ( mode_info_now.mode != mode_info_old.mode
 			 || mode_info_now.time != mode_info_old.time
 			 || mode_info_now.frequence != mode_info_old.frequence
 			 || mode_info_now.mode == LIGHT_MODE_SHAKING
@@ -128,14 +160,12 @@ static void pattern_change_task(void * par)
 			iic_point_mic();
 			vTaskResume(light_mode_music_task_handle);
 			EMBARC_PRINTF("Enable light mode music.\r\n");
-		}
-		else if (mode_info_now.mode >= MIN_MODE && mode_info_now.mode <= MAX_MODE) {
+		} else if (mode_info_now.mode >= MIN_MODE && mode_info_now.mode <= MAX_MODE) {
 			iic_point_mpu();
 			vTaskResume(mpu_task_handle);
 			vTaskResume(light_mode_task_handle);
 			EMBARC_PRINTF("Enable light mode.\r\n");
-		}
-		else
+		} else
 		EMBARC_PRINTF("The mode isn't changed.\r\n");
 		mode_info_old = mode_info_now;
 
@@ -143,17 +173,15 @@ static void pattern_change_task(void * par)
 		vTaskSuspend(NULL);
 	}
 }
-/**
- * \brief  light mode task in FreeRTOS
- * \details Give the signal of light.
- * \param[in] *par
+
+/** 
+ * \brief  	light mode task in FreeRTOS.
  */
 static void light_mode_task(void * par)
 {
+	/* Give the signal of light. */
 	mode_info mode_info_now = INIT_MODE_INFO;
-	//vTaskSuspend(NULL);
-	while(1) {	
-
+	while(1) {
 		xQueuePeek(mode_info_queue, &mode_info_now , (TickType_t) 0 );
 		EMBARC_PRINTF("mode is %d,bright is %d,freq is %d,time is %d,weather is %d \r\n",
 				mode_info_now.mode, mode_info_now.bright, mode_info_now.frequence, mode_info_now.time, mode_info_now.weather);
@@ -161,134 +189,128 @@ static void light_mode_task(void * par)
 					mode_info_now.time, mode_info_now.fance_txt, mode_info_now.weather);
 	}
 }
-/**
- * \brief  light mode music task in FreeRTOS
- * \details mic data update and signal giver to light.
- * \param[in] *par
+
+/** 
+ * \brief	light mode music task in FreeRTOS.
+ * \detail	This task should execute exactly every MIC_PERIOD_TICK.
  */
 static void light_mode_music_task(void * par)
 {
-    TickType_t xLastExecutionTime;
-    uint16_t fft_num = 0;
-	compx fft_data[fft_N];     /* fft data segment */
-	uint16_t fft_in[fft_N];    /* fft data input */
-	uint8_t fft_out[fft_N/2];  /* fft data output */
+	/* music data update and signal giver to light. */
+	TickType_t xLastExecutionTime;
+	uint16_t fft_num = 0;
+	 /* fft data segment. */
+	compx fft_data[fft_N];
+	/* fft data input */
+	uint16_t fft_in[fft_N];
+	/* fft data output */
+	uint8_t fft_out[fft_N/2];
 
-    xLastExecutionTime = xTaskGetTickCount();
-   	vTaskSuspend(NULL);
+	xLastExecutionTime = xTaskGetTickCount();
+	vTaskSuspend(NULL);
 
-    while(1) {
-    	fft_in[fft_num] = adc_get();
+	while(1) {
+		fft_in[fft_num] = adc_get();
 		if (++fft_num >= fft_N) {
 			fft_num = 0;
-	    	fft_Data(fft_in, fft_data);
-	    	fft_Convert(fft_data);				/* FFT resolve */
-	    	fft_powerMag(fft_data, fft_out);	/* data compress */
-
-	    	light_mode_music(fft_out);
-	    	//led_turn(2);														
-	 	}
-	 	led_turn(2);
-        vTaskDelayUntil( &xLastExecutionTime, MIC_PERIOD_TICK);    
-        /*!< This task should execute exactly every MIC_PERIOD_TICK. */
-    }
+			fft_Data(fft_in, fft_data);
+			fft_Convert(fft_data);			/* FFT resolve */
+			fft_powerMag(fft_data, fft_out);	/* data compress */
+			light_mode_music(fft_out);														
+		}
+		led_turn(2);
+		vTaskDelayUntil( &xLastExecutionTime, MIC_PERIOD_TICK);    
+	}
 }
-/**
- * \brief  light mode alarm task in FreeRTOS
- * \details alarm and weather.
- * \param[in] *par
+
+/** 
+ * \brief	light mode alarm task in FreeRTOS.
  */
 static void light_mode_alarm_task(void * par)
 {
-    TickType_t xLastExecutionTime;
-    mode_info mode_info_now = INIT_MODE_INFO;
-    xLastExecutionTime = xTaskGetTickCount();
+	TickType_t xLastExecutionTime;
+	mode_info mode_info_now = INIT_MODE_INFO;
+	xLastExecutionTime = xTaskGetTickCount();
 
-    while(1) {
-
-    	iic0_point_rtc();
-    	//vTaskDelay(10);
-    	if ( pcf8563_date_updata( 0, (uint8_t)(time_clock / 60), (uint8_t)(time_clock % 60)) ) {
+	while(1) {
+		iic0_point_rtc();
+		if (pcf8563_date_updata(0,(uint8_t)(time_clock / 60),(uint8_t)(time_clock % 60))) {
 			xSemaphoreGive( recognition_semaphore );
 			vTaskResume( pattern_change_task_handle );
-    		light_mode_clock();
-    		xQueuePeek(mode_info_queue, &mode_info_now , (TickType_t) 0 );
-    		mode_info_now.mode = LIGHT_MODE_WEATHER;
-    		xQueueOverwrite( mode_info_queue, &mode_info_now );
-    		vTaskResume( pattern_change_task_handle );
-    	}
-
+			xQueuePeek(mode_info_queue, &mode_info_now , (TickType_t) 0 );
+			mode_info_now.mode = LIGHT_MODE_WEATHER;
+			xQueueOverwrite( mode_info_queue, &mode_info_now );
+			vTaskResume( pattern_change_task_handle );
+		}
 		iic_point_mpu();
-
-    	EMBARC_PRINTF("alarm mode.\r\n");
-	 	led_turn(3);
-        vTaskDelayUntil( &xLastExecutionTime, ALARM_PERIOD_TICK);    
-        /*!< This task should execute exactly every MIC_PERIOD_TICK. */
-    }
+		EMBARC_PRINTF("alarm mode.\r\n");
+		led_turn(3);
+		vTaskDelayUntil( &xLastExecutionTime, ALARM_PERIOD_TICK);    
+	}
 }
-/**
- * \brief  mpu task in FreeRTOS
- * \details MPU read and imu update.
- * \param[in] *par
+
+/** 
+ * \brief	mpu task in FreeRTOS.
  */
 static void mpu_task(void * par)
 {
-    TickType_t xLastExecutionTime; 
-    uint32_t recognition_cnt = 0;
-    mode_info mode_info_now = INIT_MODE_INFO;
-    uint8_t mode_flag = 0;
-    uint8_t recognition_flag = 0;
-    xLastExecutionTime = xTaskGetTickCount();
-    while(1) {
-        //perf_start();
-    	mpu6050_updata();
-
-    	recognition_cnt++;
-    	if (recognition_cnt == REC_PERIOD_TICK/MPU_PERIOD_TICK) {
-    		recognition_cnt = 0;
-    		mode_flag = imu_pattern_recongnition();
-    		if (recognition_flag == 0 && mode_flag == ACTION_START) {
-    			recognition_flag = 1;
-    			light_mode_change_success();
-    			xQueuePeek(mode_info_queue, &mode_info_now , (TickType_t) 0 );
-    			light_mode_change_flag(mode_info_now.mode);
-    			EMBARC_PRINTF("Start of recognition.\r\n");
-    			xSemaphoreGive( recognition_semaphore );
+	/* MPU read and imu update. */
+	TickType_t xLastExecutionTime; 
+	uint32_t recognition_cnt = 0;
+	mode_info mode_info_now = INIT_MODE_INFO;
+	uint8_t mode_flag = 0;
+	uint8_t recognition_flag = 0;
+	xLastExecutionTime = xTaskGetTickCount();
+	while(1) {
+		imu_mpu6050_update();
+		recognition_cnt++;
+		if (recognition_cnt == REC_PERIOD_TICK/MPU_PERIOD_TICK) {
+			recognition_cnt = 0;
+			mode_flag = imu_pattern_recongnition();
+			if (recognition_flag == 0 && mode_flag == ACTION_START) {
+				recognition_flag = 1;
+				light_mode_change_success();
+				xQueuePeek(mode_info_queue, &mode_info_now , (TickType_t) 0 );
+				light_mode_change_flag(mode_info_now.mode);
+				EMBARC_PRINTF("Start of recognition.\r\n");
+				xSemaphoreGive( recognition_semaphore );
 				vTaskResume( pattern_change_task_handle );
-			}
-			else if (recognition_flag == 1 && mode_flag == ACTION_RIGHT) {
-				mode_info_now.mode++;
-				if ( mode_info_now.mode > MAX_MODE)
-					mode_info_now.mode = MIN_MODE;
-				light_mode_change_flag(mode_info_now.mode);
-				EMBARC_PRINTF("mode ++ %d.\r\n", mode_info_now.mode);
-			}
-			else if (recognition_flag == 1 && mode_flag == ACTION_LEFT) {
-				mode_info_now.mode--;
-				if ( mode_info_now.mode < MIN_MODE)
-					mode_info_now.mode = MAX_MODE;				
-				light_mode_change_flag(mode_info_now.mode);
-				EMBARC_PRINTF("mode -- %d.\r\n", mode_info_now.mode);
-			}
-			else if (recognition_flag == 1 && mode_flag == ACTION_OVER) {
-				recognition_flag = 0;
-				if (mode_info_now.mode >= MIN_MODE && mode_info_now.mode <= MAX_MODE) {
-					xQueueOverwrite( mode_info_queue, &mode_info_now );
-					EMBARC_PRINTF("After pattern recognition mode is %d .\r\n", mode_info_now.mode);
 				}
-				else
-					EMBARC_PRINTF("Wrong pattern recognition mode is %d .\r\n", mode_info_now.mode);
-				vTaskResume( pattern_change_task_handle );
-			}
-    	}
+				else if (recognition_flag == 1 && mode_flag == ACTION_RIGHT) {
+					mode_info_now.mode++;
+					if ( mode_info_now.mode > MAX_MODE)
+						mode_info_now.mode = MIN_MODE;
+					light_mode_change_flag(mode_info_now.mode);
+					EMBARC_PRINTF("mode ++ %d.\r\n", mode_info_now.mode);
+				}
+				else if (recognition_flag == 1 && mode_flag == ACTION_LEFT) {
+					mode_info_now.mode--;
+					if ( mode_info_now.mode < MIN_MODE)
+						mode_info_now.mode = MAX_MODE;				
+					light_mode_change_flag(mode_info_now.mode);
+					EMBARC_PRINTF("mode -- %d.\r\n", mode_info_now.mode);
+				}
+				else if (recognition_flag == 1 && mode_flag == ACTION_OVER) {
+					recognition_flag = 0;
+					if (mode_info_now.mode >= MIN_MODE && mode_info_now.mode <= MAX_MODE) {
+						xQueueOverwrite( mode_info_queue, &mode_info_now );
+						EMBARC_PRINTF("After pattern recognition mode is %d .\r\n", mode_info_now.mode);
+					}
+					else
+						EMBARC_PRINTF("Wrong pattern recognition mode is %d .\r\n", mode_info_now.mode);
+					vTaskResume( pattern_change_task_handle );
+				}
+		}
 
-        led_turn(1);
-        //mpu_cycle = perf_end();
-        vTaskDelayUntil(&xLastExecutionTime, MPU_PERIOD_TICK);    
-        /*!< This task should execute exactly every MPU_PERIOD_TICK. */
-    }
+		led_turn(1);
+		vTaskDelayUntil(&xLastExecutionTime, MPU_PERIOD_TICK);    
+		/* !< This task should execute exactly every MPU_PERIOD_TICK. */
+	}
 }
-/** create all the task at appropriate order */
+
+/** 
+ * \brief	create all the task at appropriate order.
+ */
 uint8_t task_init(void)
 {
 	/* start the timer 1 for */
@@ -306,7 +328,7 @@ uint8_t task_init(void)
 		EMBARC_PRINTF("create light mode running task error\r\n");
 		return -1;			
 	}
-	//light_mode_test();
+	
 	/* create the light mode music task */
 	if (xTaskCreate(light_mode_music_task, "light_mode_music_task", 128, (void *)1,
 			light_mode_music_task_prioroties, &light_mode_music_task_handle) != pdPASS) {
@@ -324,17 +346,12 @@ uint8_t task_init(void)
 		EMBARC_PRINTF("create mpu_task error\r\n");
 		return -1;			
 	}
-	// /* create the light mode alarm task */
-	// if (xTaskCreate(light_mode_alarm_task, "light_mode_alarm_task", 128, (void *)1,
-	// 		light_mode_alarm_task_prioroties, &light_mode_alarm_task_handle) != pdPASS) {
-	// 	EMBARC_PRINTF("create light mode alarm task error\r\n");
-	// 	return -1;			
-	// }	
-	
 	return 0;
 }
 
-/** turn around one bit of led */
+/** 
+ * \brief	turn around one bit of led.
+ */
 static void led_turn(uint8_t led)
 {
 	uint32_t temp;
@@ -342,7 +359,9 @@ static void led_turn(uint8_t led)
 	led_write(~temp, 0x001 << led);
 }
 
-/** performance timer initialization */
+/** 
+ * \brief	performance timer initialization. 
+ */
 static void perf_init(unsigned int id)
 {
 	if (timer_start(id, TIMER_CTRL_NH, 0xFFFFFFFF) < 0) {
@@ -352,7 +371,9 @@ static void perf_init(unsigned int id)
 	perf_id = id;
 }
 
-/** performance timer start */
+/** 
+ * \brief	performance timer start.
+ */
 static void perf_start(void)
 {
 	if (timer_current(perf_id, (void *)(&start)) < 0) {
@@ -360,7 +381,9 @@ static void perf_start(void)
 	}
 }
 
-/** performance timer end, and return the time passed */
+/** 
+ *\brief	performance timer end, and return the time passed.
+ */
 static unsigned int perf_end(void)
 {
 	unsigned int end = 0;
@@ -368,7 +391,6 @@ static unsigned int perf_end(void)
 	if (timer_current(perf_id, (void *)(&end)) < 0) {
 		return 0;
 	}
-
 	if (start < end) {
 		return (end - start);
 	} else {
@@ -376,6 +398,7 @@ static unsigned int perf_end(void)
 	}
 }
 
+/** @} */
 
 
 
