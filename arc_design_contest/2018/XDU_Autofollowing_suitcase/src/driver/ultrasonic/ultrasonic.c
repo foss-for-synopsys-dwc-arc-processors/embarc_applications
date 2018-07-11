8bd7f7d6c0cdc0ee490b70a0aad2d714a5903420
Mandywualmighty <39255542+Mandywualmighty@users.noreply.github.com>
2018-07-11 21:16:36 +0800

    Update ultrasonic.c

--- arc_design_contest/2018/XDU_Autofollowing_suitcase/src/driver/ultrasonic/ultrasonic.c
+++ arc_design_contest/2018/XDU_Autofollowing_suitcase/src/driver/ultrasonic/ultrasonic.c
@@ -5,8 +5,8 @@
 uint32_t dis_l,dis_r,dis_m;
 uint32_t data_m,data_l,data_r,val,echo_m,echo_l,echo_r,rx,jishu,ji1_m,ji2_m,ji1_l,ji2_l,ji1_r,ji2_r;	
 uint32_t icount,icount_flag;
-static DEV_GPIO_PTR PMOD1;//超声波
-static DEV_GPIO_PTR PMOD3_R;//超声波
+static DEV_GPIO_PTR PMOD1;
+static DEV_GPIO_PTR PMOD3_R;
 
  void delay(uint32_t z)
 {
@@ -19,15 +19,15 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
 {
 	PMOD1 = gpio_get_dev(DW_GPIO_PORT_A);
 	PMOD1->gpio_open(0xf00);
-	PMOD1->gpio_control(GPIO_CMD_SET_BIT_DIR_INPUT, (void *)0x100);//输入 J1*7/A8 Em 
-	PMOD1->gpio_control(GPIO_CMD_SET_BIT_DIR_OUTPUT, (void *)0x200);//输出 J1*8/A9 Tm
-	PMOD1->gpio_control(GPIO_CMD_SET_BIT_DIR_INPUT, (void *)0x400);//输入 J1*9/A10 El
-	PMOD1->gpio_control(GPIO_CMD_SET_BIT_DIR_OUTPUT, (void *)0x800);//输出 J1*10/A11 Tl	
+	PMOD1->gpio_control(GPIO_CMD_SET_BIT_DIR_INPUT, (void *)0x100);
+	PMOD1->gpio_control(GPIO_CMD_SET_BIT_DIR_OUTPUT, (void *)0x200);
+	PMOD1->gpio_control(GPIO_CMD_SET_BIT_DIR_INPUT, (void *)0x400);
+	PMOD1->gpio_control(GPIO_CMD_SET_BIT_DIR_OUTPUT, (void *)0x800);
 	
 	PMOD3_R = gpio_get_dev(DW_GPIO_PORT_D);
 	PMOD3_R->gpio_open(0x30);
-	PMOD3_R->gpio_control(GPIO_CMD_SET_BIT_DIR_INPUT, (void *)0x10);//输入 J3*1/C16 ER 
-	PMOD3_R->gpio_control(GPIO_CMD_SET_BIT_DIR_OUTPUT, (void *)0x20);//输出 J3*2/C17 TR
+	PMOD3_R->gpio_control(GPIO_CMD_SET_BIT_DIR_INPUT, (void *)0x10);
+	PMOD3_R->gpio_control(GPIO_CMD_SET_BIT_DIR_OUTPUT, (void *)0x20);
 }
 
 
@@ -36,8 +36,8 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
    //     timer_stop(INTNO_TIMER1);	
 		
 		PMOD1->gpio_read(&data_m, 0x800);
-		PMOD1->gpio_write(0x800, 0x800);//输出高电平
-		PMOD1->gpio_read(&data_m, 0x800);//读高电平
+		PMOD1->gpio_write(0x800, 0x800);
+		PMOD1->gpio_read(&data_m, 0x800);
 	    delay(20);
 		PMOD1->gpio_read(&data_m, 0x800);
 		PMOD1->gpio_write(0x000, 0x800);
@@ -49,7 +49,7 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
 		{
 		 PMOD1->gpio_read(&echo_m,0x400);
 		 icount++;
-		 if(icount>400000)//可以不用这么大的数值。3米对应的值就可以
+		 if(icount>400000)
 			{
 				icount_flag=1;
 				break;
@@ -66,7 +66,7 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
 		if(icount_flag==0)
 		{
 			dis_m=ji2_m*0.00085;
-			EMBARC_PRINTF("dis_m:%d\n", dis_m);//检验
+			EMBARC_PRINTF("dis_m:%d\n", dis_m);
 		}	
 		return dis_m;
 }
@@ -75,19 +75,19 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
 {
     //    timer_stop(INTNO_TIMER1);	
 		PMOD1->gpio_read(&data_l, 0x200);
-		PMOD1->gpio_write(0x200, 0x200);//输出高电平
-		PMOD1->gpio_read(&data_l, 0x200);//读高电平
+		PMOD1->gpio_write(0x200, 0x200);
+		PMOD1->gpio_read(&data_l, 0x200);
         delay(20);
 		PMOD1->gpio_read(&data_l, 0x200);	
-		PMOD1->gpio_write(0x000, 0x200);//输出高电平
-		PMOD1->gpio_read(&data_l, 0x200);//读高电平	
+		PMOD1->gpio_write(0x000, 0x200);
+		PMOD1->gpio_read(&data_l, 0x200);
 		delay(30);
 		icount=0;icount_flag=0;
 		while(!(echo_l==0x100))
 		{
 		 PMOD1->gpio_read(&echo_l,0x100);
 		 icount++;
-		 if(icount>400000)//可以不用这么大的数值。3米对应的值就可以
+		 if(icount>400000)
 			{
 				icount_flag=1;
 				break;
@@ -98,14 +98,14 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
 			while(!(echo_l==0x000))
 			{
 				PMOD1->gpio_read(&echo_l,0x100);
-				//EMBARC_PRINTF("echo:%d\n", echo_l);//检验
+				//EMBARC_PRINTF("echo:%d\n", echo_l);
 			}
 		timer_current(TIMER_1, &ji2_l);	
 		ji2_l=ji2_l-ji1_l;
 		if(icount_flag==0)
 		{
 			dis_l=ji2_l*0.00085;
-			EMBARC_PRINTF("dis_l:%d\n", dis_l);//检验
+			EMBARC_PRINTF("dis_l:%d\n", dis_l);
 		}	
 		return dis_l;
 }
@@ -114,8 +114,8 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
 {
         //   timer_stop(INTNO_TIMER1);	
 	       PMOD3_R->gpio_read(&data_r, 0x20);
-	       PMOD3_R->gpio_write(0x20, 0x20);//输出高电平
-	       PMOD3_R->gpio_read(&data_r, 0x20);//读高电平
+	       PMOD3_R->gpio_write(0x20, 0x20);
+	       PMOD3_R->gpio_read(&data_r, 0x20);
 	       delay(20);
 	       PMOD3_R->gpio_read(&data_r, 0x20);
 	       PMOD3_R->gpio_write(0x00, 0x20);
@@ -126,7 +126,7 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
 			{
 			 PMOD3_R->gpio_read(&echo_r,0x10);
 			 icount++;
-			 if(icount>400000)//可以不用这么大的数值。3米对应的值就可以
+			 if(icount>400000)
 				{
 					icount_flag=1;
 					break;
@@ -143,7 +143,7 @@ static DEV_GPIO_PTR PMOD3_R;//超声波
 			if(icount_flag==0)
 			{
 				dis_r=ji2_r*0.00085;
-				EMBARC_PRINTF("dis_r:%d\n", dis_r);//检验
+				EMBARC_PRINTF("dis_r:%d\n", dis_r);
 			}	
 			return dis_r;
 }
