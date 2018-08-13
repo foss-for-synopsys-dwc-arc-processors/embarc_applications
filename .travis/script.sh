b22bd40aae8c87d2027026cac4204117f204efe1
Jingru Wang <jingru@synopsys.com>
2018-08-13 11:08:05 +0800

    travis: Add the path of expected file to python script
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -23,9 +23,11 @@ set -x
     unzip ../applications.zip || die
     bash apply_embARC_patch.sh || die
     # cd ../ || die
+
+    echo $EXPECTED
     cd .travis || die
     {
-        BUILD_OPTS="OSP_ROOT=${OSP_ROOT} TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE} GNU_VER=${GNU_VER} EXAMPLES=${EXAMPLES}"
+        BUILD_OPTS="OSP_ROOT=${OSP_ROOT} TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE} GNU_VER=${GNU_VER} EXAMPLES=${EXAMPLES} EXPECTED=${EXPECTED}"
         python build.py ${BUILD_OPTS} || die
     }
 
