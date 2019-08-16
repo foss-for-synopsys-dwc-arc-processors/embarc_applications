91cd9c7baf91476a5fa1205cb1c6618bb8119cfc
Jingru <jingru@synopsys.com>
2019-08-16 10:41:52 +0800

    travis: modify code_check.sh
    Signed-off-by: Jingru <jingru@synopsys.com>

--- .ci/code_check.sh
+++ .ci/code_check.sh
@@ -18,7 +18,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
         | while read file; do flake8 "${file}" >> result.log 2>&1; done
 
 
-    COMMENT_CONTENT=$(sed 's/$/&<br>/g' build/result.log)
+    COMMENT_CONTENT=$(sed 's/$/&<br>/g' result.log)
     COMMENT_HEAD="# Sphinx link check result \n***********************\n<pre>"
     COMMENT_TAIL="</pre>"
     COMMENT="${COMMENT_HEAD}${COMMENT_CONTENT}${COMMENT_TAIL}"
